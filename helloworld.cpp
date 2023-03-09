2271852054f7e571059d47783737ad790ed58ac8
Milan Vujic <vujic017@gmail.com>
2023-03-09 18:53:41 +0100

    Added more to helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 
+class TestClass 
+{
+    int _testNum;
+
+    public:
+
+        TestClass( int testNumber )
+        {   
+            _testNum =  testNumber;
+        }
+
+        ~TestClass(){
+            std::cout << "Obj deleted" << std::endl;
+        }
+
+}
 int main(){
 
     std::cout << "Hello World!" << std::endl;
     
+    for (int i = 0; i < 99; ++i)
+        i *= i;
+
+    std::cout << i << std::endl;
+    
+    TestClass testObj(i);
+
+    TestClass *ptr = nullptr;
+
+    ptr = new TestClass(i/2);
+
+    delete ptr;
+
     return 0;
 }
